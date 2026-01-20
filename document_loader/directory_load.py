e4cefb76083fef123e178781bdfb214a1579829a
newuserAJ <adithjose58@gmail.com>
2026-01-20 12:06:25 +0530

    Directory Loader

---
+++ document_loader/directory_load.py
@@ -0,0 +1,27 @@
+'''The time to compute increases with the number of documents and pages in the directory and is loadedd on to the memory. 
+Therefore we require better options of the purpose of loading documents from a directory.
+
+For better load time we can use lazy_loading which loads the documents one at a time when required instead of 
+loading all at once into the memory.
+'''
+
+from langchain_community.document_loaders import DirectoryLoader,PyPDFLoader
+
+#specified the path, type of document to extract and the type of loader class to use which is PyPDFLoader in this case
+loaderr=DirectoryLoader(path='BOOKS',glob='*.pdf',loader_cls=PyPDFLoader)  
+
+docs=loaderr.lazy_load()
+
+# print(len(docs))  #Print the number of documents aka the no. of total pages loaded from all pdfs in the directory
+
+#Print the content of the first page of the first document 
+# print(docs.page_content)
+
+# #Print the metadata of the first page of the first document which includes name,type etc.
+# print(docs[0].metadata)
+
+#Using lazy loading to load documents one at a time
+#lazy_loader=DirectoryLoader(path='BOOKS',glob='*.pdf',loader_cls=PyPDFLoader,lazy_load=True)
+
+for doc in docs:
+    print(doc.metadata)
--- document_loader/py_pdf.py
+++ document_loader/py_pdf.py
@@ -1,16 +1,10 @@
 from langchain_community.document_loaders import PyPDFLoader
-from langchain_groq import ChatGroq
-from langchain_core.output_parsers import StrOutputParser
-from dotenv import load_dotenv
-from langchain_core.prompts import PromptTemplate
 
-load_dotenv()
-
-model=ChatGroq(model='llama-3.1-8b-instant', temperature=0)
-parser=StrOutputParser()
-prompt=PromptTemplate(template='give a detailed analysis of the following document:\n {document}',input_variables=['document'])
 
 loaderr=PyPDFLoader('dl-curriculum.pdf')
 docs=loaderr.load()
 print(len(docs))  # Print number of pages loaded
 print(docs[0].page_content)  #Print  content of the first page
+print(docs[1].metadata)  #Print the metadata of second page
+
+
